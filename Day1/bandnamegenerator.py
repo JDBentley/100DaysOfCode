827570ac346db306b05e4a042819e7b8b6f4ed92
Jesse Bentley <75673224+JDBentley@users.noreply.github.com>
2024-06-11 14:16:04 -0400

    Finished BNG
    Finished band name generator

---
+++ Day1/bandnamegenerator.py
@@ -0,0 +1,13 @@
+#Create a band name generator.
+#Take input of a pet name and town. Combine them for the result.
+print('Welcome to the band name generator.')
+
+#Getting inputs
+pet_name = input("What's the name of your favorite pet? ")
+town_name = input("What is the name of the town where you grew up? ")
+
+#Adding the two strings together
+band_name = pet_name + town_name
+
+#Printing results
+print(band_name)
