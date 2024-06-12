d738205695164bacf895a4497d1f6131ebab7b25
Jesse Bentley <75673224+JDBentley@users.noreply.github.com>
2024-06-12 06:23:58 -0400

    Update datatypes.py
    Finished data types code exercise.

---
+++ Day2/datatypes.py
@@ -0,0 +1,14 @@
+#Take input of a two digit number then add the two numbers together.
+#Taking input
+two_digit_number = int(input('What is your two digit number? '))
+
+#Splicing number
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+#Adding both together
+result = first_digit + second_digit
+
+#Printing final answer
+print(result)
+
